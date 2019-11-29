f1343d673a3c2e807876d8f16de2f73b0b93cc69
dkiefer <daniel.kiefer.dk@gmail.com>
2019-11-29 14:52:47 -0500

    Revert "starting to add pokemon list"
    This reverts commit aa49ccbdde1bf02509fd66d977615009e7147e80.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
 
-class App extends Component {
-  state = {
-    pokemon: []
-  };
-
-  // componentDidMount = () => {
-  //   const Pokedex = require("pokeapi-js-wrapper");
-  //   const P = new Pokedex.Pokedex();
-
-  //   P.getPokemonsList().then(response => {
-  //     this.setState({
-  //       pokemon: response.results
-  //     });
-  //   });
-  // };
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Home pokemonList={this.state.pokemon} />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,29 +1,41 @@
 import React from "react";
 import "./Home.css";
-import Pokemon from "./Pokemon.js";
 
 class Home extends React.Component {
-  //   const random = Math.floor(Math.random() * 808);
+  constructor() {
+    super();
+    this.state = {
+      pokemon: "",
+      spriteUrl: ""
+    };
+  }
 
-  //   P.getPokemonByName(random).then(response => {
-  //     this.setState({
-  //       pokemon: response.species.name,
-  //       spriteUrl: response.sprites.front_default
-  //     });
-  //   });
-  // };
+  componentDidMount = () => {
+    const Pokedex = require("pokeapi-js-wrapper");
+    const P = new Pokedex.Pokedex();
+    const random = Math.floor(Math.random() * 808);
+
+    P.getPokemonByName(random).then(response => {
+      this.setState({
+        pokemon: response.species.name,
+        spriteUrl: response.sprites.front_default
+      });
+    });
+  };
 
   render() {
     return (
-      <div className="container fluid">
+      <div className="container">
         <div className="jumbotron pb-1">
           <h1>Coming soon!</h1>A web app to quickly look up useful information
           about a pokemon to help you complete your dex!
-        </div>
-        <div className="row">
-          {this.props.pokemonList.map(pokemon => (
-            <Pokemon key={pokemon.name} pokemon={pokemon} />
-          ))}
+          <div className="row align-items-end pt-3">
+            <img
+              className="mx-auto d-block"
+              src={this.state.spriteUrl}
+              alt={this.state.pokemon}
+            />
+          </div>
         </div>
       </div>
     );
